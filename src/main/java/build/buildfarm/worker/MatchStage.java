96639b7fb17d8503e107e7fd942ab0e1903ed5c9
George Gensure <ggensure@uber.com>
2017-09-21 12:24:40 -0400

    Refactor worker into staged pipeline
    Move the major operations - Input Fetch, Execute, Writeback, of the
    worker into a pipeline with concurrent stage execution. The number of
    available concurrent slots for the execution stage is configurable, and
    each stage will stall on a transition to the next if it is (fully)
    occupied.

---
+++ src/main/java/build/buildfarm/worker/MatchStage.java
@@ -0,0 +1,84 @@
+// Copyright 2017 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package build.buildfarm.worker;
+
+import com.google.devtools.remoteexecution.v1test.Action;
+import com.google.devtools.remoteexecution.v1test.ExecuteOperationMetadata;
+import com.google.longrunning.Operation;
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+class MatchStage extends PipelineStage {
+  MatchStage(Worker worker, PipelineStage output, PipelineStage error) {
+    super(worker, output, error);
+  }
+
+  @Override
+  protected void iterate() throws InterruptedException {
+    if (!output.claim()) {
+      return;
+    }
+    worker.instance.match(worker.config.getPlatform(), worker.config.getRequeueOnFailure(), (operation) -> {
+      return fetch(operation);
+    });
+  }
+
+  private boolean fetch(Operation operation) {
+    ExecuteOperationMetadata metadata;
+    Action action;
+    try {
+      metadata = operation.getMetadata().unpack(ExecuteOperationMetadata.class);
+      action = Action.parseFrom(worker.instance.getBlob(metadata.getActionDigest()));
+    } catch (InvalidProtocolBufferException ex) {
+      return false;
+    }
+    Path execDir = worker.root.resolve(operation.getName());
+    output.offer(new OperationContext(
+        operation,
+        execDir,
+        metadata,
+        action,
+        new ArrayList<>()));
+    return true;
+  }
+
+  @Override
+  public OperationContext take() throws InterruptedException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean claim() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void release() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void offer(OperationContext operation) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void setInput(PipelineStage input) {
+    throw new UnsupportedOperationException();
+  }
+}
---
+++ src/main/java/build/buildfarm/worker/Pipeline.java
@@ -0,0 +1,100 @@
+// Copyright 2017 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package build.buildfarm.worker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Pipeline {
+  private final Map<PipelineStage, Thread> stageThreads;
+  private final Map<PipelineStage, Integer> stageClosePriorities;
+  // FIXME ThreadGroup?
+
+  Pipeline() {
+    stageThreads = new HashMap<>();
+    stageClosePriorities = new HashMap<>();
+  }
+
+  public void add(PipelineStage stage, int closePriority) {
+    stageThreads.put(stage, new Thread(stage));
+    if (closePriority < 0) {
+      throw new IllegalArgumentException("closePriority cannot be negative");
+    }
+    stageClosePriorities.put(stage, closePriority);
+  }
+
+  void start() {
+    for (Thread stageThread : stageThreads.values()) {
+      stageThread.start();
+    }
+  }
+
+  void join() {
+    List<PipelineStage> inactiveStages = new ArrayList<>();
+    boolean closeStage = false;
+    boolean wasInterrupted = false;
+    try {
+      while (!stageThreads.isEmpty()) {
+        if (closeStage) {
+          PipelineStage stageToClose = null;
+          int maxPriority = -1;
+          for (PipelineStage stage : stageThreads.keySet()) {
+            if (stage.isClosed()) {
+              stageToClose = null;
+              break;
+            }
+            if (stageClosePriorities.get(stage) > maxPriority) {
+              maxPriority = stageClosePriorities.get(stage);
+              stageToClose = stage;
+            }
+          }
+          if (stageToClose != null && !stageToClose.isClosed()) {
+            System.out.println("Closing stage at priority " + maxPriority);
+            stageToClose.close();
+          }
+        }
+        for (Map.Entry<PipelineStage, Thread> stageThread : stageThreads.entrySet()) {
+          PipelineStage stage = stageThread.getKey();
+          Thread thread = stageThread.getValue();
+          try {
+            thread.join(closeStage ? 1 : 1000);
+          } catch (InterruptedException ex) {
+            wasInterrupted = true;
+          }
+
+          if (!thread.isAlive()) {
+            System.out.println("Stage has exited at priority " + stageClosePriorities.get(stage));
+            inactiveStages.add(stage);
+          } else if (stage.isClosed()) {
+            System.out.println("Interrupting unterminated closed thread at priority " + stageClosePriorities.get(stage));
+            thread.interrupt();
+          }
+        }
+        closeStage = false;
+        for (PipelineStage stage : inactiveStages) {
+          stageThreads.remove(stage);
+          closeStage = true;
+        }
+        inactiveStages.clear();
+      }
+    } finally {
+      if (wasInterrupted) {
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
+}
