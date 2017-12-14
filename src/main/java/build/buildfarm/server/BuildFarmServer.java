40fa77cf05a5dbf2354ea0932882e4cf5b9d9d7d
George Gensure <werkt@users.noreply.github.com>
2017-12-14 10:42:14 -0500

    Register Operations Service (#32)
    Provide longrunning operations grpc service in the buildfarm suite.
    Add a test which ensures OperationsGrpc service implementation

--- src/main/java/build/buildfarm/server/BuildFarmServer.java
+++ src/main/java/build/buildfarm/server/BuildFarmServer.java
@@ -58,6 +58,7 @@ public class BuildFarmServer {
         .addService(new ByteStreamService(instances))
         .addService(new ExecutionService(instances))
         .addService(new OperationQueueService(instances))
+        .addService(new OperationsService(instances))
         .addService(new WatcherService(instances))
         .build();
   }
