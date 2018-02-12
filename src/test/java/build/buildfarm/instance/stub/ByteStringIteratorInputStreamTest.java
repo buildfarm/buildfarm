8b9a8d1f07485c14eb6dff924306944c9d95d573
George Gensure <ggensure@uber.com>
2018-02-12 13:34:35 -0500

    Adopt testing suite from bazel
    In the interest of modularizing tests, I've adapted bazel's test suite
    mechanism to our testing, and moved our previously available tests into
    this model.

--- src/test/java/build/buildfarm/instance/stub/ByteStringIteratorInputStreamTest.java
+++ src/test/java/build/buildfarm/instance/stub/ByteStringIteratorInputStreamTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package build.buildfarm;
+package build.buildfarm.instance.stub;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -22,7 +22,9 @@ import com.google.common.collect.Iterators;
 import com.google.protobuf.ByteString;
 import java.io.IOException;
 import java.io.InputStream;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -115,5 +117,52 @@ public class ByteStringIteratorInputStreamTest {
     assertThat(ByteString.copyFrom(buffer)).isEqualTo(ByteString.copyFrom(expected));
     assertThat(in.read()).isEqualTo(3);
     assertThat(in.read()).isEqualTo(-1);
+  }
+
+  @Test
+  public void iteratorAtBeginningHasAvailable() throws IOException {
+    InputStream in = new ByteStringIteratorInputStream(ImmutableList.<ByteString>of().iterator());
+    assertThat(in.available()).isEqualTo(0);
+  }
+
+  @Test
+  public void iteratorAtEndIsUnavailable() throws IOException {
+    InputStream in = new ByteStringIteratorInputStream(ImmutableList.<ByteString>of().iterator());
+    assertThat(in.available()).isEqualTo(0);
+  }
+
+  @Rule
+  public final ExpectedException exception = ExpectedException.none();
+
+  @Test
+  public void closedStreamAvailableThrowsIOException() throws IOException {
+    InputStream in = new ByteStringIteratorInputStream(
+        ImmutableList.<ByteString>of(
+            ByteString.copyFromUtf8("Hello, World")).iterator());
+    in.close();
+    exception.expect(IOException.class);
+    in.available();
+    assertThat(true).isFalse();
+  }
+
+  @Test
+  public void closedStreamReadDefaultThrowsIOException() throws IOException {
+    InputStream in = new ByteStringIteratorInputStream(
+        ImmutableList.<ByteString>of(
+            ByteString.copyFromUtf8("Hello, World")).iterator());
+    in.close();
+    exception.expect(IOException.class);
+    in.read();
+  }
+
+  @Test
+  public void closedStreamReadThrowsIOException() throws IOException {
+    InputStream in = new ByteStringIteratorInputStream(
+        ImmutableList.<ByteString>of(
+            ByteString.copyFromUtf8("Hello, World")).iterator());
+    in.close();
+    exception.expect(IOException.class);
+    byte[] buffer = new byte[5];
+    in.read(buffer, 0, 5);
   }
 }
--- src/test/java/build/buildfarm/server/BuildFarmServerTest.java
+++ src/test/java/build/buildfarm/server/BuildFarmServerTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package build.buildfarm;
+package build.buildfarm.server;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -31,10 +31,10 @@ import build.buildfarm.v1test.PollOperationRequest;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.devtools.remoteexecution.v1test.Action;
+import com.google.devtools.remoteexecution.v1test.ActionCacheGrpc;
 import com.google.devtools.remoteexecution.v1test.BatchUpdateBlobsRequest;
 import com.google.devtools.remoteexecution.v1test.BatchUpdateBlobsResponse;
 import com.google.devtools.remoteexecution.v1test.Command;
-import com.google.devtools.remoteexecution.v1test.ContentAddressableStorageGrpc;
 import com.google.devtools.remoteexecution.v1test.Digest;
 import com.google.devtools.remoteexecution.v1test.Directory;
 import com.google.devtools.remoteexecution.v1test.ExecuteOperationMetadata;
@@ -42,6 +42,7 @@ import com.google.devtools.remoteexecution.v1test.ExecuteRequest;
 import com.google.devtools.remoteexecution.v1test.ExecutionGrpc;
 import com.google.devtools.remoteexecution.v1test.FindMissingBlobsRequest;
 import com.google.devtools.remoteexecution.v1test.FindMissingBlobsResponse;
+import com.google.devtools.remoteexecution.v1test.GetActionResultRequest;
 import com.google.devtools.remoteexecution.v1test.UpdateBlobRequest;
 import com.google.devtools.remoteexecution.v1test.ContentAddressableStorageGrpc;
 import com.google.longrunning.CancelOperationRequest;
@@ -119,10 +120,10 @@ public class BuildFarmServerTest {
 
   @Test
   public void findMissingBlobs() {
-    DigestUtil sha1DigestUtil = new DigestUtil(DigestUtil.HashFunction.SHA256);
+    DigestUtil digestUtil = new DigestUtil(DigestUtil.HashFunction.SHA256);
     ByteString content = ByteString.copyFromUtf8("Hello, World!");
     Iterable<Digest> digests =
-        Collections.singleton(sha1DigestUtil.compute(content));
+        Collections.singleton(digestUtil.compute(content));
     FindMissingBlobsRequest request = FindMissingBlobsRequest.newBuilder()
         .setInstanceName("memory")
         .addAllBlobDigests(digests)
@@ -156,7 +157,7 @@ public class BuildFarmServerTest {
     BatchUpdateBlobsResponse.Response expected = BatchUpdateBlobsResponse.Response.newBuilder()
         .setBlobDigest(digest)
         .setStatus(com.google.rpc.Status.newBuilder()
-            .setCode(com.google.rpc.Code.OK.getNumber())
+            .setCode(Code.OK.getNumber())
             .build())
         .build();
     assertThat(response.getResponsesList())
@@ -331,5 +332,26 @@ public class BuildFarmServerTest {
         ExecutionGrpc.newBlockingStub(inProcessChannel);
 
     return executeStub.execute(executeRequest);
+  }
+
+  @Test
+  public void actionNotCached() {
+    DigestUtil digestUtil = new DigestUtil(DigestUtil.HashFunction.SHA256);
+    GetActionResultRequest request = GetActionResultRequest.newBuilder()
+        .setInstanceName("memory")
+        .setActionDigest(digestUtil.empty())
+        .build();
+
+    ActionCacheGrpc.ActionCacheBlockingStub actionCacheStub =
+        ActionCacheGrpc.newBlockingStub(inProcessChannel);
+
+    StatusRuntimeException notFound = null;
+    try {
+      actionCacheStub.getActionResult(request);
+    } catch (StatusRuntimeException e) {
+      notFound = e;
+    }
+    assertThat(notFound).isNotNull();
+    assertThat(notFound.getStatus().getCode()).isEqualTo(io.grpc.Status.Code.NOT_FOUND);
   }
 }
