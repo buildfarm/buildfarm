b2eef9afb7e9c4884504bf3c2193457e8194cd64
George Gensure <werkt@users.noreply.github.com>
2018-03-06 14:56:52 -0500

    Adding memory-instance-tests (#123)
    adds mockito dependency

---
+++ src/test/java/build/buildfarm/instance/memory/ByteStringStreamSourceTest.java
@@ -0,0 +1,116 @@
+// Copyright 2018 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package build.buildfarm.instance.memory;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RunWith(JUnit4.class)
+public class ByteStringStreamSourceTest {
+  @Test
+  public void closeCallsOnCloseOnce() throws IOException {
+    Runnable mockOnClose = mock(Runnable.class);
+    ByteStringStreamSource source = new ByteStringStreamSource(mockOnClose);
+    verify(mockOnClose, never()).run();
+    source.getOutputStream().close();
+    verify(mockOnClose, times(1)).run();
+  }
+
+  @Test
+  public void closeShouldSetIsClosed() throws IOException {
+    ByteStringStreamSource source = new ByteStringStreamSource(() -> {});
+    assertThat(source.isClosed()).isFalse();
+    source.getOutputStream().close();
+    assertThat(source.isClosed()).isTrue();
+  }
+
+  @Test
+  public void openStreamCanStreamUpdates() throws IOException {
+    ByteStringStreamSource source = new ByteStringStreamSource(() -> {});
+    InputStream inputStream = source.openStream();
+    assertThat(inputStream.available()).isEqualTo(0);
+    source.getOutputStream().write('a');
+    assertThat(inputStream.available()).isEqualTo(1);
+    assertThat(inputStream.read()).isEqualTo('a');
+  }
+
+  @Test
+  public void readAtClosedEnd() throws IOException {
+    ByteStringStreamSource source = new ByteStringStreamSource(() -> {});
+    InputStream inputStream = source.openStream();
+    source.getOutputStream().close();
+    assertThat(inputStream.read()).isEqualTo(-1);
+  }
+
+
+  @Test
+  public void skipNegativePreventsSkip() throws IOException {
+    ByteStringStreamSource source = new ByteStringStreamSource(() -> {});
+    InputStream inputStream = source.openStream();
+    assertThat(inputStream.skip(-1)).isEqualTo(0);
+  }
+
+  @Test
+  public void skipBlocksForInput() throws IOException, InterruptedException {
+    ByteStringStreamSource source = new ByteStringStreamSource(() -> {});
+    InputStream inputStream = source.openStream();
+    Thread thread = new Thread(() -> {
+      try {
+        assertThat(inputStream.skip(1)).isEqualTo(1);
+      } catch (IOException e) {
+        fail("Unexpected IOException: " + e);
+      }
+    });
+    thread.start();
+    while (!thread.isAlive()) {
+      Thread.sleep(10);
+    }
+    source.getOutputStream().write('a');
+    while (thread.isAlive()) {
+      Thread.sleep(10);
+    }
+  }
+
+  @Test
+  public void readBlocksForInput() throws IOException, InterruptedException {
+    ByteStringStreamSource source = new ByteStringStreamSource(() -> {});
+    InputStream inputStream = source.openStream();
+    Thread thread = new Thread(() -> {
+      try {
+        assertThat(inputStream.read()).isEqualTo('a');
+      } catch (IOException e) {
+        fail("Unexpected IOException: " + e);
+      }
+    });
+    thread.start();
+    while (!thread.isAlive()) {
+      Thread.sleep(10);
+    }
+    source.getOutputStream().write('a');
+    while (thread.isAlive()) {
+      Thread.sleep(10);
+    }
+  }
+}
---
+++ src/test/java/build/buildfarm/instance/memory/MemoryLRUContentAddressableStorageTest.java
@@ -0,0 +1,72 @@
+// Copyright 2018 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package build.buildfarm.instance.memory;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import build.buildfarm.instance.memory.MemoryLRUContentAddressableStorage;
+import build.buildfarm.common.ContentAddressableStorage;
+import build.buildfarm.common.ContentAddressableStorage.Blob;
+import build.buildfarm.common.DigestUtil;
+import com.google.devtools.remoteexecution.v1test.Digest;
+import com.google.protobuf.ByteString;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class MemoryLRUContentAddressableStorageTest {
+  @Test
+  public void expireShouldCallOnExpiration() {
+    ContentAddressableStorage storage = new MemoryLRUContentAddressableStorage(10);
+
+    DigestUtil sha1DigestUtil = new DigestUtil(DigestUtil.HashFunction.SHA256);
+    Runnable mockOnExpiration = mock(Runnable.class);
+    storage.put(new Blob(ByteString.copyFromUtf8("stdout"), sha1DigestUtil), mockOnExpiration);
+    verify(mockOnExpiration, never()).run();
+    storage.put(new Blob(ByteString.copyFromUtf8("stderr"), sha1DigestUtil));
+    verify(mockOnExpiration, times(1)).run();
+  }
+
+  @Test
+  public void expireShouldOccurAtLimitExactly() {
+    ContentAddressableStorage storage = new MemoryLRUContentAddressableStorage(11);
+
+    DigestUtil sha1DigestUtil = new DigestUtil(DigestUtil.HashFunction.SHA256);
+    Runnable mockOnExpiration = mock(Runnable.class);
+    storage.put(new Blob(ByteString.copyFromUtf8("stdin"), sha1DigestUtil), mockOnExpiration);
+    storage.put(new Blob(ByteString.copyFromUtf8("stdout"), sha1DigestUtil), mockOnExpiration);
+    verify(mockOnExpiration, never()).run();
+    storage.put(new Blob(ByteString.copyFromUtf8("a"), sha1DigestUtil));
+    verify(mockOnExpiration, times(1)).run();
+  }
+
+  @Test
+  public void duplicateEntryRegistersMultipleOnExpiration() {
+    ContentAddressableStorage storage = new MemoryLRUContentAddressableStorage(10);
+
+    DigestUtil sha1DigestUtil = new DigestUtil(DigestUtil.HashFunction.SHA256);
+    Runnable mockOnExpiration = mock(Runnable.class);
+    storage.put(new Blob(ByteString.copyFromUtf8("stdout"), sha1DigestUtil), mockOnExpiration);
+    storage.put(new Blob(ByteString.copyFromUtf8("stdout"), sha1DigestUtil), mockOnExpiration);
+    verify(mockOnExpiration, never()).run();
+    storage.put(new Blob(ByteString.copyFromUtf8("stderr"), sha1DigestUtil));
+    verify(mockOnExpiration, times(2)).run();
+  }
+}
