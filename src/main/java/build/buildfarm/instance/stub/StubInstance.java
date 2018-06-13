d52ab01f85200ea739b4b67e095685463d62f1c2
George Gensure <werkt@users.noreply.github.com>
2018-06-13 10:04:51 -0400

    XS Suppress empty blob requests from stub clients (#156)
    * Suppress empty blob gets and puts from stub clients
    
    Fixes #154 error hierarchy

--- src/main/java/build/buildfarm/instance/stub/StubInstance.java
+++ src/main/java/build/buildfarm/instance/stub/StubInstance.java
@@ -265,6 +265,9 @@ public class StubInstance implements Instance {
 
   @Override
   public ByteString getBlob(Digest blobDigest) {
+    if (blobDigest.getSizeBytes() == 0) {
+      return ByteString.EMPTY;
+    }
     try (InputStream in = newStreamInput(getBlobName(blobDigest))) {
       return ByteString.readFrom(in);
     } catch (IOException ex) {
@@ -280,6 +283,9 @@ public class StubInstance implements Instance {
   @Override
   public Digest putBlob(ByteString blob)
       throws IOException, IllegalArgumentException, InterruptedException {
+    if (blob.size() == 0) {
+      return digestUtil.empty();
+    }
     Digest digest = digestUtil.compute(blob);
     Chunker chunker = new Chunker(blob, digest);
     uploader.uploadBlobs(Collections.singleton(chunker));
