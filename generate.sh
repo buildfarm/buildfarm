7164f4ef05b3fd563ea220dedd81f7bf593c3dd7
George Gensure <ggensure@uber.com>
2018-11-14 10:53:01 -0500

    Extend generate to run target build and server

--- generate.sh
+++ generate.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set -e
+bazel coverage $@
 COVERAGE=bazel-testlogs/coverage
 mkdir -p $COVERAGE
 traces=$(find bazel-testlogs/ -name coverage.dat)
 rm -fr $COVERAGE/*
 ln -s $PWD/src/main/java/build $COVERAGE/build
 genhtml -f -o $COVERAGE $traces
+cd $COVERAGE
+python -m SimpleHTTPServer
