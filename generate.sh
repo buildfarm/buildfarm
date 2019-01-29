c94d8a572828765f7e2e70782661a6e17e7b45b0
George Gensure <ggensure@uber.com>
2019-01-29 12:14:37 -0500

    Adapt genhtml use for later version

--- generate.sh
+++ generate.sh
@@ -4,9 +4,9 @@ set -e
 bazel coverage $@
 COVERAGE=bazel-testlogs/coverage
 mkdir -p $COVERAGE
-traces=$(find bazel-testlogs/ -name coverage.dat)
+traces=$(find bazel-testlogs/ -name coverage.dat | sed "s|^|$PWD/|")
 rm -fr $COVERAGE/*
 ln -s $PWD/src/main/java/build $COVERAGE/build
-genhtml -f -o $COVERAGE $traces
 cd $COVERAGE
+genhtml -f $traces
 python -m SimpleHTTPServer
