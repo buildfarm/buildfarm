f9a7e41e3e3fb3ed37f59cf5b3a1077d2d4067c8
Jianqiu <jianqium@uber.com>
2020-05-29 16:52:20 -0400

    adjust output message

--- src/main/java/build/buildfarm/Cat.java
+++ src/main/java/build/buildfarm/Cat.java
@@ -633,7 +633,7 @@ class Cat {
             strNumFormat, "Number of Evicted Entries", response.getCasEvictedEntryCount()));
     System.out.println(
         String.format(
-            strNumFormat, "Total size of Evicted Entries", response.getCasEvictedEntrySize()));
+            strNumFormat, "Total Evicted Entries size in Bytes", response.getCasEvictedEntrySize()));
 
     String strStrFormat = "%-50s : %s";
     System.out.println(
