ebca72dd1ded60d0d7cc370154936825bbe46dde
George Gensure <werkt@users.noreply.github.com>
2020-06-25 15:21:48 -0400

    Revert incorrectly included shard change (#435)

--- src/main/java/build/buildfarm/instance/shard/ShardInstance.java
+++ src/main/java/build/buildfarm/instance/shard/ShardInstance.java
@@ -1364,23 +1364,7 @@ public class ShardInstance extends AbstractServerInstance {
     checkState(digest.getSizeBytes() == content.size());
     SettableFuture<Long> writtenFuture = SettableFuture.create();
     Write write = getBlobWrite(digest, UUID.randomUUID(), requestMetadata);
-    write.addListener(
-        () -> {
-          try {
-            long committedSize = write.getCommittedSize();
-            if (committedSize != digest.getSizeBytes()) {
-              logger.warning(
-                  format(
-                      "committed size %d did not match expectation for %s, ignoring it",
-                      committedSize, DigestUtil.toString(digest)));
-            }
-            writtenFuture.set(digest.getSizeBytes());
-          } catch (RuntimeException e) {
-            // should we wrap with an additional exception?
-            writtenFuture.setException(e);
-          }
-        },
-        directExecutor());
+    write.addListener(() -> writtenFuture.set(digest.getSizeBytes()), directExecutor());
     try (OutputStream out = write.getOutput(60, SECONDS, () -> {})) {
       content.writeTo(out);
     } catch (IOException e) {
