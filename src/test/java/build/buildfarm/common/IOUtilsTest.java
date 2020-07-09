82ed5fb709639c07f9e2637e8d7ad036588ef43f
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-07-09 17:17:05 -0400

    disable test

--- src/test/java/build/buildfarm/common/IOUtilsTest.java
+++ src/test/java/build/buildfarm/common/IOUtilsTest.java
@@ -30,7 +30,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class IOUtilsTest {
+class IOUtilsTest {
   private Path root;
 
   private FileStore fileStore;
