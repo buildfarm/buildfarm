6a335454d81795b4764690cbd8e09f8964f26d40
Yuriy Belenitsky <yuriyb@uber.com>
2020-08-26 10:20:38 -0400

    Add getHosts grpc service (#496)
    * Add getHosts grpc service
    
    * Downgrade log message
    
    * Refactor log messages

--- src/main/java/build/buildfarm/common/admin/aws/AwsAdmin.java
+++ src/main/java/build/buildfarm/common/admin/aws/AwsAdmin.java
@@ -15,16 +15,28 @@
 package build.buildfarm.common.admin.aws;
 
 import build.buildfarm.common.admin.Admin;
+import build.buildfarm.v1test.GetHostsResult;
+import build.buildfarm.v1test.Host;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Filter;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
 import com.amazonaws.services.simplesystemsmanagement.model.SendCommandRequest;
+import com.google.protobuf.util.Timestamps;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 import java.util.logging.Logger;
 
 public class AwsAdmin implements Admin {
@@ -47,7 +59,7 @@ public class AwsAdmin implements Admin {
   public void stopContainer(String hostId, String containerName) {
     String stopContainerCmd =
         "docker ps | grep " + containerName + " | awk '{print $1 }' | xargs -I {} docker stop {}";
-    Map<String, List<String>> parameters = new HashMap<String, List<String>>();
+    Map<String, List<String>> parameters = new HashMap<>();
     parameters.put("commands", Collections.singletonList(stopContainerCmd));
     ssm.sendCommand(
         new SendCommandRequest()
@@ -55,5 +67,46 @@ public class AwsAdmin implements Admin {
             .withInstanceIds(hostId)
             .withParameters(parameters));
     logger.info(String.format("Stopped container: %s on host: %s", containerName, hostId));
+  }
+
+  @Override
+  public GetHostsResult getHosts(String filter, int ageInMinutes, String status) {
+    GetHostsResult.Builder resultBuilder = GetHostsResult.newBuilder();
+    List<Host> hosts = new ArrayList<>();
+    DescribeInstancesResult instancesResult =
+        ec2.describeInstances(
+            new DescribeInstancesRequest()
+                .withFilters(new Filter().withName("tag-value").withValues(filter)));
+    Long hostNum = 1L;
+    for (Reservation r : instancesResult.getReservations()) {
+      for (Instance e : r.getInstances()) {
+        Long uptime = getHostUptimeInMinutes(e.getLaunchTime());
+        if (e.getPrivateIpAddress() != null
+            && uptime > ageInMinutes
+            && status.equalsIgnoreCase(e.getState().getName())) {
+          Host.Builder hostBuilder = Host.newBuilder();
+          hostBuilder.setHostNum(hostNum++);
+          hostBuilder.setDnsName(e.getPrivateDnsName());
+          hostBuilder.setHostId(e.getInstanceId());
+          hostBuilder.setIpAddress(e.getPrivateIpAddress());
+          hostBuilder.setLaunchTime(Timestamps.fromMillis(e.getLaunchTime().getTime()));
+          hostBuilder.setLifecycle(e.getInstanceLifecycle());
+          hostBuilder.setNumCores(e.getCpuOptions().getCoreCount());
+          hostBuilder.setState(e.getState().getName());
+          hostBuilder.setType(e.getInstanceType());
+          hostBuilder.setUptimeMinutes(uptime);
+          hosts.add(hostBuilder.build());
+        }
+      }
+    }
+    resultBuilder.addAllHosts(hosts);
+    resultBuilder.setNumHosts(hosts.size());
+    logger.fine(String.format("Got %d hosts for filter: %s", hosts.size(), filter));
+    return resultBuilder.build();
+  }
+
+  private long getHostUptimeInMinutes(Date launchTime) {
+    Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+    return (cal.getTime().getTime() - launchTime.getTime()) / 60000;
   }
 }
