7edb30d8cad5324f373ab2befe4673095e32591e
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-09-22 12:14:13 -0400

    debug ci outpt

--- src/test/java/build/buildfarm/common/IOUtilsTest.java
+++ src/test/java/build/buildfarm/common/IOUtilsTest.java
@@ -57,6 +57,9 @@ public class IOUtilsTest {
     assertThat(files.size()).isEqualTo(2);
     Object firstKey = files.get(0).fileKey();
     Object secondKey = files.get(1).fileKey();
+    
+    System.out.println(firstKey);
+    System.out.println(secondKey);  
     assertThat(firstKey).isEqualTo(secondKey);
   }
 
@@ -74,6 +77,8 @@ public class IOUtilsTest {
     assertThat(files.size()).isEqualTo(2);
     Object firstKey = files.get(0).fileKey();
     Object secondKey = files.get(1).fileKey();
+    System.out.println(firstKey);
+    System.out.println(secondKey);  
     assertThat(firstKey).isNotEqualTo(secondKey);
   }
 }
