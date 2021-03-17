d3b2533dd584a7ae6b0d11ba61e346e278cae35d
Trevor Hickey <thickey@uber.com>
2021-03-17 10:17:16 -0400

    keep private data mems by adding accessors

--- src/main/java/build/buildfarm/cas/DigestMismatchException.java
+++ src/main/java/build/buildfarm/cas/DigestMismatchException.java
@@ -19,11 +19,23 @@ import build.buildfarm.common.DigestUtil;
 import java.io.IOException;
 
 public class DigestMismatchException extends IOException {
+  private final Digest actual;
+  private final Digest expected;
 
   public DigestMismatchException(Digest actual, Digest expected) {
     super(
         String.format(
             "computed digest %s does not match expected %s",
             DigestUtil.toString(actual), DigestUtil.toString(expected)));
+    this.actual = actual;
+    this.expected = expected;
+  }
+
+  public Digest getActual() {
+    return actual;
+  }
+
+  public Digest getExpected() {
+    return expected;
   }
 }
