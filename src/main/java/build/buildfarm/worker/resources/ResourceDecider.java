59d525980366c720870bbc64bfd28be4727b78d2
Trevor Hickey <thickey@uber.com>
2021-04-19 10:07:43 -0400

    add ensure claims are not 0

--- src/main/java/build/buildfarm/worker/resources/ResourceDecider.java
+++ src/main/java/build/buildfarm/worker/resources/ResourceDecider.java
@@ -69,6 +69,11 @@ public class ResourceDecider {
     // Claim minimal core amount with regards to execute stage width.
     limits.cpu.limit = (limits.cpu.min > 0 || limits.cpu.max > 0);
     limits.cpu.claimed = Math.min(limits.cpu.min, executeStageWidth);
+    
+    // Claims cannot be 0 as they are used by Execute Stage.
+    if (limits.cpu.claimed == 0){
+      limits.cpu.claimed = 1;
+    }
 
     // Should we limit the memory of the action during execution? by default, no.
     // If the action has suggested memory restrictions on itself, then yes.
