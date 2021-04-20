2a63e4a22eaca4ad03f834b339b8e075134e9f72
Trevor Hickey <thickey@uber.com>
2021-04-20 00:30:13 -0400

    different port

--- .bazelci/test_buildfarm_container.sh
+++ .bazelci/test_buildfarm_container.sh
@@ -79,4 +79,4 @@ cat server.log
 
 # Build bazel targets with buildfarm
 cd src/test/many;
-MANY_CC_BINARIES=50 MANY_CC_LIBRARIES=2 MANY_CC_LIBRARY_SOURCES=1 ./../../../bazelw build :cc --remote_executor=grpc://localhost:8080
+MANY_CC_BINARIES=50 MANY_CC_LIBRARIES=2 MANY_CC_LIBRARY_SOURCES=1 ./../../../bazelw build :cc --remote_executor=grpc://localhost:8880
