27578d76c7152bc4f4f314a35d690ec87742f9ff
Trevor Hickey <TrevorJamesHickey@gmail.com>
2021-05-05 16:46:04 -0400

    allow using linux sandbox in the executor (#770)

--- src/main/java/build/buildfarm/worker/shard/ShardWorkerContext.java
+++ src/main/java/build/buildfarm/worker/shard/ShardWorkerContext.java
@@ -38,6 +38,7 @@ import build.buildfarm.common.DigestUtil;
 import build.buildfarm.common.DigestUtil.ActionKey;
 import build.buildfarm.common.EntryLimitException;
 import build.buildfarm.common.InputStreamFactory;
+import build.buildfarm.common.LinuxSandboxOptions;
 import build.buildfarm.common.Poller;
 import build.buildfarm.common.Size;
 import build.buildfarm.common.Write;
@@ -927,8 +928,29 @@ class ShardWorkerContext implements WorkerContext {
     }
 
     // Possibly set network restrictions.
-    if (limits.network.blockNetwork) {
+    // This is not the ideal implementation of block-network.
+    // For now, without the linux-sandbox, we will unshare the network namespace.
+    if (limits.network.blockNetwork && !limits.useLinuxSandbox) {
       arguments.add("/usr/bin/unshare", "-n", "-r");
+    }
+
+    // Decide the CLI for running the sandbox
+    // For reference on how bazel spawns the sandbox:
+    // https://github.com/bazelbuild/bazel/blob/ddf302e2798be28bb67e32d5c2fc9c73a6a1fbf4/src/main/java/com/google/devtools/build/lib/sandbox/LinuxSandboxUtil.java#L183
+    if (limits.useLinuxSandbox) {
+
+      // Choose the sandbox which is built and deployed with the worker image.
+      arguments.add("/app/buildfarm/linux-sandbox");
+
+      // Construct the CLI options for this binary.
+      LinuxSandboxOptions options = new LinuxSandboxOptions();
+      options.createNetns = limits.network.blockNetwork;
+
+      // Pass flags based on the sandbox CLI options.
+      if (options.createNetns) {
+        arguments.add("-N");
+      }
+      arguments.add("--");
     }
 
     // Decide the CLI for running under cgroups
