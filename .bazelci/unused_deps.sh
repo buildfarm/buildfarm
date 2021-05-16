264600bbe0dd5b2a35dcb3173d9a0eb8972698fb
Trevor Hickey <thickey@uber.com>
2021-05-16 02:21:37 -0400

    updates

---
+++ .bazelci/unused_deps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run from the root of repository.
+# This script will remove any unused java dependencies
+
+TOOL_URL=https://github.com/bazelbuild/buildtools/releases/download/4.0.1/unused_deps-darwin-amd64
+LOCAL_TOOL="unused_deps"
