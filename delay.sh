7e04893114c288da1243cecf04ec3f98d566c5c2
Trevor Hickey <thickey@uber.com>
2021-06-02 13:37:54 -0400

    updates

--- delay.sh
+++ delay.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Delay running a program by sleeping.
+# If you are also skipping sleep syscalls, this will result in timeshifting you into the future.
+# This can help catch problems by tricking the program into thinking its future time.
 sleep $1
 "${@: 2}";
