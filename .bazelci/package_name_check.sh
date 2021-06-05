789844253746d6d43b4ad15de81d9b1a4cc345fd
Trevor Hickey <thickey@uber.com>
2021-06-05 16:05:42 -0400

    updates

--- .bazelci/package_name_check.sh
+++ .bazelci/package_name_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Run from the root of repository.
-# This script will analyze all source files to ensure their package names are specified correctly.
-# Bazel coverage does not work properly if the package name does not match the file layout.
+# Run this from the root of repository.
+# The script will check that each java source file has the a package name that matches its file path.
+# This is done for more than just consistency. Bazel's code coverage seems to exclude files when the package name does not match.
 
-# Print an error such that it will surface in the context of buildkite
+# Print an error such that it will surface in the context of buildkite.
 print_error() {
     >&2 echo "$1"
     if [ -v BUILDKITE ] ; then
@@ -20,23 +20,46 @@ extract_package_name_from_source() {
 }
 derive_package_name_from_file() {
         local package_name=$1
+        
+        # remove path prefix
         package_name=${package_name#"src/main/java/"}
         package_name=${package_name#"src/test/java/"}
+        
+        # remove file name
         package_name="${package_name%/*}"
+        
+        # turn path substring into package
+        package_name="${package_name////$'.'}"
         echo $package_name
 }
 
-# Get all of the files to analyze
+# Get all of the files to analyze.
 files=$(find src/* -type f -name "*.java")
 
+# Collect all the files whose package statements do not match expected format.
+incorrect_files=()
 for file in $files
 do
         current_package_name=$(extract_package_name_from_source $file)
         expected_package_name=$(derive_package_name_from_file $file)
-	if [ -z "$current_package_name" ]
+         if [ "$current_package_name" != "$expected_package_name" ]
 	then
-	      echo "\$var is empty" $expected_package_name
-	else
-	      echo "\$var is NOT empty" $expected_package_name
+	      incorrect_files+=($file)
 	fi
 done
+
+
+# If there are no package issues, we complete successfully.
+if [ ${#incorrect_files[@]} -eq 0 ]; then
+    echo "All files have expected package format.";
+    exit 0;
+fi;
+
+
+# If there are package issues, we report them and fail overall.
+print_error "There are files whose package statement does not match their file path:";
+for value in "${incorrect_files[@]}"
+do
+     print_error $value
+done
+exit -1;
