3195aeec4bab605235578570d155191d7fab2163
Trevor Hickey <thickey@uber.com>
2021-06-30 14:37:45 -0400

    updates

---
+++ .bazelci/run_abseil_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Test bazel targets with buildfarm
+cd src/test/abseil;
+./../../../bazelw test --test_tag_filters=-benchmark @com_google_absl//... --remote_executor=grpc://localhost:8980
