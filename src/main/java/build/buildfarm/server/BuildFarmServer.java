77f24049a95f6227650cfe368329a1d33357c627
Kristian Kosev <kkosev@vmware.com>
2021-08-12 18:39:50 +0300

    add copyright notices

--- src/main/java/build/buildfarm/server/BuildFarmServer.java
+++ src/main/java/build/buildfarm/server/BuildFarmServer.java
@@ -1,3 +1,5 @@
+//“Copyright 2021 VMware, Inc.”
+//SPDX-License-Identifier: Apache-2.0
 // Copyright 2017 The Bazel Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
