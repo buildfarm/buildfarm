29ab8f90fadddf9d36c9ec06b7f1294b222878cd
Trevor Hickey <thickey@uber.com>
2021-09-20 13:51:26 -0400

    updates

--- .bazelci/rpm_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Ensure that RPM packages can be built.
-# Currently this is done inside a container since rules_pkg has a system dependency on rpmbuild
-docker build -t buildfarm .
-docker run buildfarm /bin/bash -c "cd buildfarm; ./bazelw build --host_force_python=PY2 //src/main/java/build/buildfarm/rpms/worker:buildfarm-worker-rpm"
