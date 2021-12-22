3cc21448a836639c93c848e4c6a4a9967490b0e3
Trevor Hickey <thickey@uber.com>
2021-12-21 22:51:18 -0500

    updates

--- src/main/java/build/buildfarm/cas/cfc/CASFileCache.java
+++ src/main/java/build/buildfarm/cas/cfc/CASFileCache.java
@@ -381,6 +381,10 @@ public abstract class CASFileCache implements ContentAddressableStorage {
     return parseFileEntryKey(fileName, size, digestUtil);
   }
 
+  boolean blobExistsOnLocalStorage(String key) {
+    return Files.exists(getPath(key));
+  }
+
   private boolean contains(
       Digest digest,
       boolean isExecutable,
@@ -393,7 +397,7 @@ public abstract class CASFileCache implements ContentAddressableStorage {
         result.mergeFrom(digest).setSizeBytes(entry.size);
       }
       onContains.accept(key);
-      return true;
+      return blobExistsOnLocalStorage(key);
     }
     return false;
   }
