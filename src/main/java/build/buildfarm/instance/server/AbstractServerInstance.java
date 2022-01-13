f702f4f9dfa1c3619e1f1483db3dfbb509bf6e67
Trevor Hickey <thickey@uber.com>
2022-01-13 00:27:08 -0500

    updates

--- src/main/java/build/buildfarm/instance/server/AbstractServerInstance.java
+++ src/main/java/build/buildfarm/instance/server/AbstractServerInstance.java
@@ -332,8 +332,15 @@ public abstract class AbstractServerInstance implements Instance {
   private static boolean shouldEnsureOutputsPresent(
       boolean ensureOutputsPresent, RequestMetadata requestMetadata) {
 
-    // We will ensure outputs are present if the system is globally configured to check.
-    // Otherwise we will determine if dynamically from optional URI parameters.
+    // The 'ensure outputs present' setting means that the AC will only return results to the client
+    // when all of the action output blobs are present in the CAS.  If any one blob is missing, the
+    // system will return a cache miss.  Although this is a more expensive check to perform, some
+    // users may want to enable this feature. It may be useful if you cannot rely on requestMetadata
+    // of incoming messages (perhaps due to a proxy). Or other build systems may not be reliable
+    // without this extra check.
+
+    // We perform the outputs present check if the system is globally configured to check for it.
+    // Otherwise the behavior is determined dynamically from optional URI parameters.
     if (ensureOutputsPresent) {
       return true;
     }
