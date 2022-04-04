d08b92eb4c7d785ec2dd74f24f7eb23fae324298
Trevor Hickey <thickey@uber.com>
2022-04-04 12:34:56 -0400

    try mounting bazel for docker tests

--- .bazelci/docker_unit_test.sh
+++ .bazelci/docker_unit_test.sh
@@ -3,4 +3,4 @@
 
 # Build a container for unit tests and run them
 docker build -t buildfarm .
-docker run buildfarm /bin/bash -c "cd buildfarm; ./bazelw test --build_tests_only ..."
+docker run buildfarm --v "/usr/local/bin/bazel:/usr/local/bin/bazel" /bin/bash -c "cd buildfarm; ./bazelw test --build_tests_only ..."
