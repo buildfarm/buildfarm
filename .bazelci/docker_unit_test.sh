4aacc69222a493b820fb49385197b1d80dcb4ff9
Trevor Hickey <thickey@uber.com>
2022-04-05 00:53:00 -0400

    updates

--- .bazelci/docker_unit_test.sh
+++ .bazelci/docker_unit_test.sh
@@ -4,4 +4,4 @@
 # Build a container for unit tests and run them
 cp `which bazel` bazel
 docker build -t buildfarm .
-docker run /bin/bash -c "ls; ls buildfarm; cd buildfarm; ./bazel test --build_tests_only ..."
+docker run buildfarm /bin/bash -c "ls; ls buildfarm; cd buildfarm; ./bazel test --build_tests_only ..."
