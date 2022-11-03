58026408aefd4d7d67ef25249990c552b96fdbd1
Yuriy Belenitsky <yuriy@80degreeswest.com>
2022-11-03 19:09:22 -0400

    Reduce amount of execution metadata being logged to completed operations only (#1214)

--- src/main/java/build/buildfarm/common/config/Metrics.java
+++ src/main/java/build/buildfarm/common/config/Metrics.java
@@ -11,18 +11,16 @@ public class Metrics {
   }
 
   public enum LOG_LEVEL {
-    OFF,
     SEVERE,
     WARNING,
     INFO,
     FINE,
     FINER,
     FINEST,
-    ALL
   }
 
   private PUBLISHER publisher = PUBLISHER.LOG;
-  private LOG_LEVEL logLevel = LOG_LEVEL.OFF;
+  private LOG_LEVEL logLevel = LOG_LEVEL.FINEST;
   private String topic;
   private int topicMaxConnections;
   private String secretName;
