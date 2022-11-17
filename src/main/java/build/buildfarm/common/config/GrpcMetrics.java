2a0fc7700c5654c5cbb2318fcd4aa02c75aab533
Dmitriy Shirchenko <shirchen@users.noreply.github.com>
2022-11-17 11:17:56 -0800

    Add optional GRPC metrics for worker (#1219)
    * add additional grpc metrics for workers
    
    * fix import
    
    * appease buildifier
    
    * refactor code to combine usages b/n server and worker
    
    * simplify deps
    
    * mvoe to BuildfarmConfigs
    
    * Revert "mvoe to BuildfarmConfigs"
    
    This reverts commit d068c98faccd57b334123a3b1d8fc6978d09d558.

--- src/main/java/build/buildfarm/common/config/GrpcMetrics.java
+++ src/main/java/build/buildfarm/common/config/GrpcMetrics.java
@@ -1,9 +1,33 @@
 package build.buildfarm.common.config;
 
+import io.grpc.ServerBuilder;
 import lombok.Data;
+import me.dinowernli.grpc.prometheus.Configuration;
+import me.dinowernli.grpc.prometheus.MonitoringServerInterceptor;
 
 @Data
 public class GrpcMetrics {
   private boolean enabled = false;
   private boolean provideLatencyHistograms = false;
+
+  public static void handleGrpcMetricIntercepts(
+      ServerBuilder<?> serverBuilder, GrpcMetrics grpcMetrics) {
+    // Decide how to capture GRPC Prometheus metrics.
+    // By default, we don't capture any.
+    if (grpcMetrics.isEnabled()) {
+      // Assume core metrics.
+      // Core metrics include send/receive totals tagged with return codes.  No latencies.
+      Configuration grpcConfig = Configuration.cheapMetricsOnly();
+
+      // Enable latency buckets.
+      if (grpcMetrics.isProvideLatencyHistograms()) {
+        grpcConfig = grpcConfig.allMetrics();
+      }
+
+      // Apply config to create an interceptor and apply it to the GRPC server.
+      MonitoringServerInterceptor monitoringInterceptor =
+          MonitoringServerInterceptor.create(grpcConfig);
+      serverBuilder.intercept(monitoringInterceptor);
+    }
+  }
 }
