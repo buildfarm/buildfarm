7e88d9556f3e8bf568d5e5238f4a1d6c81355e01
Trevor Hickey <thickey@uber.com>
2022-12-21 22:00:06 -0500

    Update src/main/java/build/buildfarm/common/config/Cas.java

--- src/main/java/build/buildfarm/common/config/Cas.java
+++ src/main/java/build/buildfarm/common/config/Cas.java
@@ -19,7 +19,7 @@ public class Cas {
 
   private TYPE type = TYPE.FILESYSTEM;
   private String path = "cache";
-  private long maxSizeBytes = 2147483648L; // 2 * 1024 * 1024 * 1024
+  private long maxSizeBytes = 0;
   private boolean fileDirectoriesIndexInMemory = false;
   private boolean skipLoad = false;
   private String target;
