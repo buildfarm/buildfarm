f1dc95407e6b47d325bcab5b589641c14af3669a
Win Wang <wang.win0+git@gmail.com>
2023-01-17 23:36:07 -0500

    Add tests for Persistent Workers
    Co-authored-by: Shane Delmore <shane@delmore.io>

---
+++ persistentworkers/src/test/java/persistent/bazel/processes/ProtoWorkerRWTest.java
@@ -0,0 +1,60 @@
+package persistent.bazel.processes;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.google.devtools.build.lib.worker.WorkerProtocol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import persistent.common.processes.JavaProcessWrapper;
+import persistent.common.processes.ProcessWrapper;
+import persistent.testutil.ProcessUtils;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static persistent.testutil.ProcessUtils.spawnPersistentWorkerProcess;
+
+@RunWith(JUnit4.class)
+public class ProtoWorkerRWTest {
+
+  // Similar to AdderTest; but spawns the process via a jar
+  @SuppressWarnings("CheckReturnValue")
+  @Test
+  public void canAddMultipleTimesWithAdder() throws Exception {
+    Path workDir = Files.createTempDirectory("test-workdir-");
+
+    String filename = "adder-bin_deploy.jar";
+
+    Path jarPath = ProcessUtils.retrieveFileResource(
+        getClass().getClassLoader(),
+        filename,
+        workDir.resolve(filename)
+    );
+
+    ProcessWrapper process;
+    try (JavaProcessWrapper jpw = spawnPersistentWorkerProcess(jarPath, "adder.Adder")) {
+      process = jpw;
+      ProtoWorkerRW rw = new ProtoWorkerRW(jpw);
+
+      assertThat(jpw.isAlive()).isTrue();
+
+      rw.write(WorkerProtocol.WorkRequest.newBuilder().addArguments("1").addArguments("3").build());
+      assertThat(rw.waitAndRead().getOutput()).isEqualTo("4");
+      assertThat(jpw.isAlive()).isTrue();
+
+      rw.write(WorkerProtocol.WorkRequest.newBuilder().addArguments("2").addArguments("5").build());
+      assertThat(rw.waitAndRead().getOutput()).isEqualTo("7");
+      assertThat(jpw.isAlive()).isTrue();
+    }
+
+    // try-with-resources done -> close() called, process should have been destroyForicbly()'d
+    assertThat(process).isNotNull();
+    process.waitFor();
+
+    assertThat(process.isAlive()).isFalse();
+    assertThat(process.exitValue()).isNotEqualTo(0);
+  }
+}
