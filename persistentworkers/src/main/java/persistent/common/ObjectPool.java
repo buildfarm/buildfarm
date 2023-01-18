6fd7b13cd92c7fedbbf8f5771df993fd78cb85c9
Win Wang <wang.win0+git@gmail.com>
2023-01-17 23:36:07 -0500

    Introduce Persistent Workers common API
    Co-authored-by: Shane Delmore <shane@delmore.io>

---
+++ persistentworkers/src/main/java/persistent/common/ObjectPool.java
@@ -0,0 +1,8 @@
+package persistent.common;
+
+public interface ObjectPool<K, V> {
+
+  V obtain(K key) throws Exception;
+
+  void release(K key, V obj);
+}
---
+++ persistentworkers/src/main/java/persistent/common/util/Args.java
@@ -0,0 +1,39 @@
+package persistent.common.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Args {
+
+  public static final String ARGSFILE_START = "@";
+
+  public static final String ARGSFILE_ESCAPE = "@@";
+
+  public static boolean isArgsFile(String arg) {
+    return arg.startsWith(ARGSFILE_START) && !arg.startsWith(ARGSFILE_ESCAPE);
+  }
+
+  // Expand arguments of argsfiles
+  public static List<String> expandArgsfiles(List<String> args) throws IOException {
+    List<String> res = new ArrayList<>();
+    for (String arg : args) {
+      if (isArgsFile(arg)) {
+        res.addAll(readAllLines(arg));
+      } else {
+        res.add(arg);
+      }
+    }
+    return res;
+  }
+
+  public static List<String> readAllLines(String fileArg) throws IOException {
+    String file = isArgsFile(fileArg)
+        ? fileArg.substring(1)
+        : fileArg;
+
+    return Files.readAllLines(Paths.get(file));
+  }
+}
