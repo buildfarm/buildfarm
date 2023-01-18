8e5ba4e973060ab8d9e077ab84ec611532d2bc14
Win Wang <wang.win0+git@gmail.com>
2023-01-17 23:36:07 -0500

    Add tests for Persistent Workers
    Co-authored-by: Shane Delmore <shane@delmore.io>

---
+++ persistentworkers/src/test/java/persistent/bazel/processes/TestDepsTest.java
@@ -0,0 +1,34 @@
+package persistent.bazel.processes;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import persistent.testutil.ProcessUtils;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(JUnit4.class)
+public class TestDepsTest {
+
+  @SuppressWarnings("CheckReturnValue")
+  @Test
+  public void canRetrieveAdderBinDeployJar() throws Exception {
+    Path workDir = Files.createTempDirectory("test-workdir-");
+
+    String filename = "adder-bin_deploy.jar";
+
+    Path jarPath = ProcessUtils.retrieveFileResource(
+        getClass().getClassLoader(),
+        filename,
+        workDir.resolve(filename)
+    );
+
+    assertThat(Files.exists(jarPath)).isTrue();
+
+    assertThat(Files.size(jarPath)).isAtLeast(11000000L); // at least 11mb
+  }
+}
---
+++ persistentworkers/src/test/java/persistent/testutil/ProcessUtils.java
@@ -0,0 +1,50 @@
+package persistent.testutil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.IOUtils;
+
+import persistent.bazel.client.PersistentWorker;
+import persistent.common.processes.JavaProcessWrapper;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class ProcessUtils {
+
+  /**
+   * Creates a new java process with the specified classpath and classname (with the main method),
+   *  passing along the --persistent_worker flag.
+   */
+  public static JavaProcessWrapper spawnPersistentWorkerProcess(String classpath, String className) throws IOException {
+    JavaProcessWrapper jpw = new JavaProcessWrapper(
+        Paths.get("."),
+        classpath,
+        className,
+        new String[]{PersistentWorker.PERSISTENT_WORKER_FLAG}
+    );
+    assertThat(jpw.isAlive()).isTrue();
+    return jpw;
+  }
+
+  public static JavaProcessWrapper spawnPersistentWorkerProcess(Path jar, String className) throws IOException {
+    return spawnPersistentWorkerProcess(jar.toString(), className);
+  }
+
+  public static JavaProcessWrapper spawnPersistentWorkerProcess(String classpath, Class<?> clazz) throws IOException {
+    String className = clazz.getPackage().getName() + "." + clazz.getSimpleName();
+    return spawnPersistentWorkerProcess(classpath, className);
+  }
+
+  public static Path retrieveFileResource(ClassLoader classLoader, String filename, Path targetPath) throws IOException {
+
+    InputStream is = classLoader.getResourceAsStream(filename);
+
+    Files.write(targetPath, IOUtils.toByteArray(is));
+
+    return targetPath;
+  }
+}
