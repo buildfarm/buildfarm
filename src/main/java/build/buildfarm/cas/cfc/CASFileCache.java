e9615a2b6f1a1eedf18c47f75214af0aeab73484
Trevor Hickey <thickey@uber.com>
2023-02-20 20:00:06 -0500

    [cas] allow copy fallback when out of hardlinks (#1273)

--- src/main/java/build/buildfarm/cas/cfc/CASFileCache.java
+++ src/main/java/build/buildfarm/cas/cfc/CASFileCache.java
@@ -60,6 +60,7 @@ import build.buildfarm.common.Write;
 import build.buildfarm.common.Write.CompleteWrite;
 import build.buildfarm.common.ZstdCompressingInputStream;
 import build.buildfarm.common.ZstdDecompressingOutputStream;
+import build.buildfarm.common.config.BuildfarmConfigs;
 import build.buildfarm.common.io.CountingOutputStream;
 import build.buildfarm.common.io.Directories;
 import build.buildfarm.common.io.FeedbackOutputStream;
@@ -99,6 +100,7 @@ import java.nio.file.FileStore;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.AbstractMap;
@@ -137,9 +139,16 @@ public abstract class CASFileCache implements ContentAddressableStorage {
   private static final Gauge casEntryCountMetric =
       Gauge.build().name("cas_entry_count").help("Number of entries in the CAS.").register();
 
+  private static final Gauge casCopyFallbackMetric =
+      Gauge.build()
+          .name("cas_copy_fallback")
+          .help("Number of times the CAS performed a file copy because hardlinking failed")
+          .register();
+
   protected static final String DEFAULT_DIRECTORIES_INDEX_NAME = "directories.sqlite";
   protected static final String DIRECTORIES_INDEX_NAME_MEMORY = ":memory:";
 
+  private BuildfarmConfigs configs = BuildfarmConfigs.getInstance();
   private final Path root;
   private final EntryPathStrategy entryPathStrategy;
   private final long maxSizeInBytes;
@@ -1931,8 +1940,7 @@ public abstract class CASFileCache implements ContentAddressableStorage {
             transformAsync(
                 put(fileNode.getDigest(), fileNode.getIsExecutable(), service),
                 (cacheFilePath) -> {
-                  // FIXME this can die with 'too many links'... needs some cascading fallout
-                  Files.createLink(filePath, cacheFilePath);
+                  linkCachedFile(filePath, cacheFilePath);
                   // we saw null entries in the built immutable list without synchronization
                   synchronized (inputsBuilder) {
                     inputsBuilder.add(key);
@@ -1963,6 +1971,38 @@ public abstract class CASFileCache implements ContentAddressableStorage {
                     Files.createSymbolicLink(symlinkPath, relativeTargetPath);
                     return symlinkPath;
                   }));
+    }
+  }
+
+  private void linkCachedFile(Path filePath, Path cacheFilePath) throws IOException {
+    // = Hardlink Limitations =
+    // Creating hardlinks is fast and saves space within the CAS.
+    // However, some filesystems such as ext4 have a total hardlink limit of 65k for individual
+    // files. Hitting this limit is easier than you think because the hardlinking occurs across
+    // actions.  A recommended filesystem to back the CAS is XFS, due to its high link counts limits
+    // per inode. If you are using a filesystem with low hardlink limits, this call will likely fail
+    // with 'Too many links...`.
+
+    try {
+      Files.createLink(filePath, cacheFilePath);
+    } catch (IOException e) {
+      // propagate the exception if we do not want to perform the fallback strategy.
+      // The client should expect a failed action with an explanation of 'Too many links...`.
+      if (!configs.getWorker().getStorages().get(0).isExecRootCopyFallback()) {
+        throw e;
+      }
+
+      // = Fallback Strategy =
+      // Buildfarm provides a configuration fallback that copies files in the event
+      // that hardlinking fails.  If you are copying files more often than hardlinking,
+      // you're performance may degrade significantly.  Therefore we provide a metric
+      // signal to allow detection of this fallback.
+      Files.copy(cacheFilePath, filePath, StandardCopyOption.REPLACE_EXISTING);
+      casCopyFallbackMetric.inc();
+
+      // TODO: A more optimal strategy would be to provide additional inodes
+      // (i.e. one backing file for a 65k or smaller link count) as a strategy,
+      // with pools of the same hash getting replicated.
     }
   }
 
