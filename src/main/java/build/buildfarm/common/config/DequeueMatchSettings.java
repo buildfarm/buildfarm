6fb3a0fe87f5eb5c06f42c583c05a858e343ce74
George Gensure <werkt@users.noreply.github.com>
2023-05-29 23:40:13 -0400

    Restore DequeueMatchSettings Platform (#1351)
    The DequeueMatchSettings Platform before 0a6802e82f provided for
    selection or rejection of queue entries based on locally selectable
    platform properties that it provides, per worker. The config transition
    eliminated this capability, and left the workers only with the
    properties selected from the queue definition restrictions. Highlighting
    this was the elimination of use of the platform during
    ShardWorkerContext test creation, leaving a platform parameter orphaned.
    
    Use the DMS Platform both in eligibility filtering and selection of the
    eligible queues to permit mildly heterogenous worker queue processing.
    
    Fixes #1350

--- src/main/java/build/buildfarm/common/config/DequeueMatchSettings.java
+++ src/main/java/build/buildfarm/common/config/DequeueMatchSettings.java
@@ -1,9 +1,11 @@
 package build.buildfarm.common.config;
 
+import build.bazel.remote.execution.v2.Platform;
 import lombok.Data;
 
 @Data
 public class DequeueMatchSettings {
   private boolean acceptEverything = true;
   private boolean allowUnmatched = false;
+  private Platform platform = Platform.getDefaultInstance();
 }
