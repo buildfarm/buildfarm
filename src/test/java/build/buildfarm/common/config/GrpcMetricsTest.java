87ad10ff39b228e51156f943da80f6ed1cd75f11
Anshuman Mishra <a.mishra@uber.com>
2023-06-16 17:15:59 -0700

    run formatter

--- src/test/java/build/buildfarm/common/config/GrpcMetricsTest.java
+++ src/test/java/build/buildfarm/common/config/GrpcMetricsTest.java
@@ -1,5 +1,10 @@
 package build.buildfarm.common.config;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import io.grpc.ServerBuilder;
 import me.dinowernli.grpc.prometheus.MonitoringServerInterceptor;
 import org.junit.Test;
@@ -7,16 +12,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 @RunWith(MockitoJUnitRunner.class)
 public class GrpcMetricsTest {
-  @Mock
-  private ServerBuilder<?> serverBuilder;
+  @Mock private ServerBuilder<?> serverBuilder;
   private final GrpcMetrics grpcMetrics = new GrpcMetrics();
 
   @Test
@@ -31,7 +29,7 @@ public class GrpcMetricsTest {
   public void testHandleGrpcMetricIntercepts_withLatencyBucket() {
     grpcMetrics.setEnabled(true);
     grpcMetrics.setProvideLatencyHistograms(true);
-    grpcMetrics.setLatencyBuckets(new double[]{1, 2, 3});
+    grpcMetrics.setLatencyBuckets(new double[] {1, 2, 3});
 
     GrpcMetrics.handleGrpcMetricIntercepts(serverBuilder, grpcMetrics);
     verify(serverBuilder, times(1)).intercept(any(MonitoringServerInterceptor.class));
