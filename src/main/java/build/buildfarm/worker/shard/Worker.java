1180a85e231feb99324001e437f1fdaf04634bd5
amishra-u <119983081+amishra-u@users.noreply.github.com>
2023-08-31 05:15:44 -0700

    Add request metadata interceptor to Worker (#1425)

--- src/main/java/build/buildfarm/worker/shard/Worker.java
+++ src/main/java/build/buildfarm/worker/shard/Worker.java
@@ -41,6 +41,7 @@ import build.buildfarm.common.config.Cas;
 import build.buildfarm.common.config.GrpcMetrics;
 import build.buildfarm.common.grpc.Retrier;
 import build.buildfarm.common.grpc.Retrier.Backoff;
+import build.buildfarm.common.grpc.TracingMetadataUtils.ServerHeadersInterceptor;
 import build.buildfarm.common.services.ByteStreamService;
 import build.buildfarm.common.services.ContentAddressableStorageService;
 import build.buildfarm.instance.Instance;
@@ -233,6 +234,7 @@ public class Worker {
               storage, inputFetchStage, executeActionStage, context, completeStage, backplane));
     }
     GrpcMetrics.handleGrpcMetricIntercepts(serverBuilder, configs.getWorker().getGrpcMetrics());
+    serverBuilder.intercept(new ServerHeadersInterceptor());
 
     return serverBuilder.build();
   }
