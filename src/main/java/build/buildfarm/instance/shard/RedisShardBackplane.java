buildfarm/buildfarm

commit 04583ec6ccd26ca97532939873a4b03aa8252c32

[configuration] fix misuse of getProcessingTimeoutMillis (#713)

src/main/java/build/buildfarm/instance/shard/RedisShardBackplane.java
@@ -30,6 +30,7 @@ import build.buildfarm.common.CommandUtils;
 import build.buildfarm.common.DigestUtil;
 import build.buildfarm.common.DigestUtil.ActionKey;
 import build.buildfarm.common.StringVisitor;
+import build.buildfarm.common.Time;
 import build.buildfarm.common.Watcher;
 import build.buildfarm.common.WorkerIndexer;
 import build.buildfarm.common.function.InterruptingRunnable;
@@ -168,6 +169,8 @@ public class RedisShardBackplane implements Backplane {
   private RedisMap actionCache;
   private RedisMap blockedActions;
   private RedisMap blockedInvocations;
+  private RedisMap processingOperations;
+  private RedisMap dispatchedOperations;
   private BalancedRedisQueue prequeue;
   private OperationQueue operationQueue;
   private CasWorkerMap casWorkerMap;
@@ -201,24 +204,6 @@ public class RedisShardBackplane implements Backplane {
     return oldOnUnsubscribe;
   }
 
-  private Instant getExpiresAt(JedisCluster jedis, String key, Instant now) {
-    String value = jedis.get(key);
-    if (value != null) {
-      try {
-        return Instant.ofEpochMilli(Long.parseLong(value));
-      } catch (NumberFormatException e) {
-        logger.log(Level.SEVERE, format("invalid expiration %s for %s", value, key));
-      }
-    }
-
-    Instant expiresAt = now.plusMillis(config.getProcessingTimeoutMillis());
-    jedis.setex(
-        key,
-        /* expire=*/ (config.getProcessingTimeoutMillis() * 2) / 1000,
-        String.format("%d", expiresAt.toEpochMilli()));
-    return expiresAt;
-  }
-
   abstract static class QueueEntryListVisitor extends StringVisitor {
     protected abstract void visit(QueueEntry queueEntry, String queueEntryJson);
 
@@ -247,6 +232,17 @@ public class RedisShardBackplane implements Backplane {
     }
   }
 
+  private Instant convertToMilliInstant(String value, String key) {
+    if (value != null) {
+      try {
+        return Instant.ofEpochMilli(Long.parseLong(value));
+      } catch (NumberFormatException e) {
+        logger.log(Level.SEVERE, format("invalid expiration %s for %s", value, key));
+      }
+    }
+    return null;
+  }
+
   private void scanProcessing(JedisCluster jedis, Consumer<String> onOperationName, Instant now) {
     prequeue.visitDequeue(
         jedis,
@@ -254,14 +250,26 @@ public class RedisShardBackplane implements Backplane {
           @Override
           protected void visit(ExecuteEntry executeEntry, String executeEntryJson) {
             String operationName = executeEntry.getOperationName();
-            String operationProcessingKey = processingKey(operationName);
+            String value = processingOperations.get(jedis, operationName);
+            int defaultTimeout_ms = config.getProcessingTimeoutMillis();
 
-            Instant expiresAt = getExpiresAt(jedis, operationProcessingKey, now);
+            // get the operation's expiration
+            Instant expiresAt = convertToMilliInstant(value, operationName);
+
+            // if expiration is invalid, add a valid one.
+            if (expiresAt == null) {
+              expiresAt = now.plusMillis(defaultTimeout_ms);
+              String keyValue = String.format("%d", expiresAt.toEpochMilli());
+              int timeout_s = Time.millisecondsToSeconds(defaultTimeout_ms);
+              processingOperations.insert(jedis, operationName, keyValue, timeout_s);
+            }
+
+            // handle expiration
             if (now.isBefore(expiresAt)) {
               onOperationName.accept(operationName);
             } else {
               if (prequeue.removeFromDequeue(jedis, executeEntryJson)) {
-                jedis.del(operationProcessingKey);
+                processingOperations.remove(jedis, operationName);
               }
             }
           }
@@ -275,14 +283,26 @@ public class RedisShardBackplane implements Backplane {
           @Override
           protected void visit(QueueEntry queueEntry, String queueEntryJson) {
             String operationName = queueEntry.getExecuteEntry().getOperationName();
-            String operationDispatchingKey = dispatchingKey(operationName);
+            String value = dispatchedOperations.get(jedis, operationName);
+            int defaultTimeout_ms = config.getDispatchingTimeoutMillis();
 
-            Instant expiresAt = getExpiresAt(jedis, operationDispatchingKey, now);
+            // get the operation's expiration
+            Instant expiresAt = convertToMilliInstant(value, operationName);
+
+            // if expiration is invalid, add a valid one.
+            if (expiresAt == null) {
+              expiresAt = now.plusMillis(defaultTimeout_ms);
+              String keyValue = String.format("%d", expiresAt.toEpochMilli());
+              int timeout_s = Time.millisecondsToSeconds(defaultTimeout_ms);
+              dispatchedOperations.insert(jedis, operationName, keyValue, timeout_s);
+            }
+
+            // handle expiration
             if (now.isBefore(expiresAt)) {
               onOperationName.accept(operationName);
             } else {
               if (operationQueue.removeFromDequeue(jedis, queueEntryJson)) {
-                jedis.del(operationDispatchingKey);
+                dispatchedOperations.remove(jedis, operationName);
               }
             }
           }
@@ -531,6 +551,8 @@ public class RedisShardBackplane implements Backplane {
     operationQueue = createOperationQueue(client, config);
     blockedActions = new RedisMap(config.getActionBlacklistPrefix());
     blockedInvocations = new RedisMap(config.getInvocationBlacklistPrefix());
+    processingOperations = new RedisMap(config.getProcessingPrefix());
+    dispatchedOperations = new RedisMap(config.getDispatchingPrefix());
 
     if (config.getSubscribeToBackplane()) {
       startSubscriptionThread();
@@ -1173,7 +1195,7 @@ public class RedisShardBackplane implements Backplane {
             format("could not remove %s from %s", operationName, prequeue.getDequeueName()));
         return null;
       }
-      jedis.del(processingKey(operationName)); // may or may not exist
+      processingOperations.remove(jedis, operationName);
       return executeEntry;
     } catch (InvalidProtocolBufferException e) {
       logger.log(Level.SEVERE, "error parsing execute entry", e);
@@ -1232,7 +1254,7 @@ public class RedisShardBackplane implements Backplane {
                 "operation %s was missing in %s, may be orphaned",
                 operationName, operationQueue.getDequeueName()));
       }
-      jedis.del(dispatchingKey(operationName)); // may or may not exist
+      dispatchedOperations.remove(jedis, operationName);
       return queueEntry;
     }
     return null;
@@ -1383,14 +1405,6 @@ public class RedisShardBackplane implements Backplane {
 
   String operationChannel(String operationName) {
     return config.getOperationChannelPrefix() + ":" + operationName;
-  }
-
-  private String processingKey(String operationName) {
-    return config.getProcessingPrefix() + ":" + operationName;
-  }
-
-  private String dispatchingKey(String operationName) {
-    return config.getDispatchingPrefix() + ":" + operationName;
   }
 
   public static String parseOperationChannel(String channel) {

src/main/java/build/buildfarm/common/Time.java
@@ -58,4 +58,15 @@ public class Time {
   public static long secondsToNanoseconds(long seconds) {
     return seconds * 1000000000;
   }
+
+  /**
+   * @brief Milliseconds to seconds.
+   * @details Milliseconds to seconds.
+   * @param milliseconds Milliseconds to convert.
+   * @return Seconds converted from milliseconds.
+   * @note Suggested return identifier: seconds.
+   */
+  public static int millisecondsToSeconds(int milliseconds) {
+    return milliseconds / 1000;
+  }
 }

commit a944dfd570ccb77c0e4c4e8011fd47ae88af0401

Dequeue flags expire after timeout effect
The flag key which contains a timeout for an operation that has been
observed in a Queue's Dequeue (atomic safeguard list, either queue or
dequeue) must not expire before the time it will be expired, and
reasonably not before the time it takes a failsafe period to identify
the timeout has elapsed. This was broken in #713, where the original
expiry was 2x the configured timeout, and reduced to just the timeout in
seconds. We now set the expiry to 1 hour or 10x the timeout, to make
unlikely an expiry clearing the flag in a working system. Commented
loudly to avoid removing the same padding again.

src/main/java/build/buildfarm/instance/shard/RedisShardBackplane.java
@@ -244,17 +244,20 @@ public class RedisShardBackplane implements Backplane {
           protected void visit(ExecuteEntry executeEntry, String executeEntryJson) {
             String operationName = executeEntry.getOperationName();
             String value = state.processingOperations.get(jedis, operationName);
-            long defaultTimeout_ms = configs.getBackplane().getProcessingTimeoutMillis();
+            long processingTimeout_ms = configs.getBackplane().getProcessingTimeoutMillis();
 
             // get the operation's expiration
             Instant expiresAt = convertToMilliInstant(value, operationName);
 
             // if expiration is invalid, add a valid one.
             if (expiresAt == null) {
-              expiresAt = now.plusMillis(defaultTimeout_ms);
+              expiresAt = now.plusMillis(processingTimeout_ms);
               String keyValue = String.format("%d", expiresAt.toEpochMilli());
-              long timeout_s = Time.millisecondsToSeconds(defaultTimeout_ms);
-              state.processingOperations.insert(jedis, operationName, keyValue, timeout_s);
+              // persist the flag for at least an hour, and at most 10 times longer than the timeout
+              // the key identifies so that we don't loop with the flag expired, resetting the
+              // unaccounted for operation
+              long expire_s = Math.max(3600, Time.millisecondsToSeconds(processingTimeout_ms) * 10);
+              state.processingOperations.insert(jedis, operationName, keyValue, expire_s);
             }
 
             // handle expiration
@@ -277,17 +280,21 @@ public class RedisShardBackplane implements Backplane {
           protected void visit(QueueEntry queueEntry, String queueEntryJson) {
             String operationName = queueEntry.getExecuteEntry().getOperationName();
             String value = state.dispatchingOperations.get(jedis, operationName);
-            long defaultTimeout_ms = configs.getBackplane().getDispatchingTimeoutMillis();
+            long dispatchingTimeout_ms = configs.getBackplane().getDispatchingTimeoutMillis();
 
             // get the operation's expiration
             Instant expiresAt = convertToMilliInstant(value, operationName);
 
             // if expiration is invalid, add a valid one.
             if (expiresAt == null) {
-              expiresAt = now.plusMillis(defaultTimeout_ms);
+              expiresAt = now.plusMillis(dispatchingTimeout_ms);
               String keyValue = String.format("%d", expiresAt.toEpochMilli());
-              long timeout_s = Time.millisecondsToSeconds(defaultTimeout_ms);
-              state.dispatchingOperations.insert(jedis, operationName, keyValue, timeout_s);
+              // persist the flag for at least an hour, and at most 10 times longer than the timeout
+              // the key identifies so that we don't loop with the flag expired, resetting the
+              // unaccounted for operation
+              long expire_s =
+                  Math.max(3600, Time.millisecondsToSeconds(dispatchingTimeout_ms) * 10);
+              state.dispatchingOperations.insert(jedis, operationName, keyValue, expire_s);
             }
 
             // handle expiration