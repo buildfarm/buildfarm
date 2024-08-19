dc18fa8e62331c76d8852d51db01def3ebf8d144
Cong Shi <shicong82@gmail.com>
2024-08-19 16:29:42 -0700

    fix(RedisShardBackplane): Don't return execute-only workers in getStorageWorkers (#1813)
    * fix(RedisShardBackplane): Don't return execute-only workers in getStorageWorkers()
    
    BuildFarm now registers all workers in RedisBackplane. When an execute-only worker is registered, RedisShardSubscriber will add it to RedisBackplane's storageWorkers. Then it's possible for getStorageWorkers() to reurn execute-only workers.
    To fix this bug, We add a field worker_type to the proto message WorkerChange.Add so that RedisShardSubscriber can tell if the worker type matches the expected value.
    
    * Cleans up code to address review comments

--- src/main/java/build/buildfarm/instance/shard/RedisShardBackplane.java
+++ src/main/java/build/buildfarm/instance/shard/RedisShardBackplane.java
@@ -470,7 +470,7 @@ public class RedisShardBackplane implements Backplane {
     dequeueService = BuildfarmExecutors.getDequeuePool();
     subscriber =
         new RedisShardSubscriber(
-            watchers, storageWorkers, configs.getBackplane().getWorkerChannel(), subscriberService);
+            watchers, storageWorkers, WorkerType.STORAGE.getNumber(), configs.getBackplane().getWorkerChannel(), subscriberService);
 
     operationSubscription =
         new RedisShardSubscription(
@@ -604,13 +604,18 @@ public class RedisShardBackplane implements Backplane {
   public void addWorker(ShardWorker shardWorker) throws IOException {
     String json = JsonFormat.printer().print(shardWorker);
     Timestamp effectiveAt = Timestamps.fromMillis(shardWorker.getFirstRegisteredAt());
+    WorkerChange.Add add =
+        WorkerChange.Add.newBuilder()
+            .setEffectiveAt(effectiveAt)
+            .setWorkerType(shardWorker.getWorkerType())
+            .build();
     String workerChangeJson =
         JsonFormat.printer()
             .print(
                 WorkerChange.newBuilder()
                     .setEffectiveAt(toTimestamp(Instant.now()))
                     .setName(shardWorker.getEndpoint())
-                    .setAdd(WorkerChange.Add.newBuilder().setEffectiveAt(effectiveAt).build())
+                    .setAdd(add)
                     .build());
     client.call(
         jedis -> {
