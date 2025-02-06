31a4ba4ba59ceafb6da78d678aa210611a8cd0dc
ynqa <un.pensiero.vano@gmail.com>
2025-02-06 11:23:28 +0900

    fix: set {min, max} idle the same as jedis default

--- src/main/java/build/buildfarm/common/config/Backplane.java
+++ src/main/java/build/buildfarm/common/config/Backplane.java
@@ -19,8 +19,8 @@ public class Backplane {
   private BACKPLANE_TYPE type = BACKPLANE_TYPE.SHARD;
   private String redisUri;
   private int jedisPoolMaxTotal = 4000;
-  private int jedisPoolMaxIdle = 4000;
-  private int jedisPoolMinIdle = 500;
+  private int jedisPoolMaxIdle = 8;
+  private int jedisPoolMinIdle = 0;
   private long jedisTimeBetweenEvictionRunsMillis = 30000L;
   private String workersHashName = "Workers";
   private String workerChannel = "WorkerChannel";
