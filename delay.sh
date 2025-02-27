55a86a395bb1d7f2a36d5b92ea446723e135f8fe
Ankit Pati <ankit.pati@salesforce.com>
2025-02-27 10:44:58 +0530

    fix: use user’s preferred `bash`
    Especially important on macOS where `/bin/bash` is antiquated.

--- delay.sh
+++ delay.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Delay running a program by sleeping.
 # If you are also skipping sleep syscalls, this will result in timeshifting you into the future.
 # This can help catch problems by tricking the program into thinking its future time.
