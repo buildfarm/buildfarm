93e697e3d62e55b36377bd34a1647aa5198f662f
Jared Neil <jaredneil@lucidchart.com>
2025-10-02 11:43:58 -0600

    Add dynamic option for reportResultStageWidth

--- src/main/java/build/buildfarm/common/config/BuildfarmConfigs.java
+++ src/main/java/build/buildfarm/common/config/BuildfarmConfigs.java
@@ -223,6 +223,7 @@ public final class BuildfarmConfigs {
 
     adjustExecuteStageWidth(configs);
     adjustInputFetchStageWidth(configs);
+    adjustReportResultStageWidth(configs);
 
     checkExecutionWrapperAvailability(configs);
   }
@@ -259,6 +260,18 @@ public final class BuildfarmConfigs {
           String.format(
               "executeInputFetchWidth modified to %d",
               configs.getWorker().getInputFetchStageWidth()));
+    }
+  }
+
+  private static void adjustReportResultStageWidth(BuildfarmConfigs configs) {
+    if (configs.getWorker().getReportResultStageWidth() == 0) {
+      configs
+          .getWorker()
+          .setReportResultStageWidth(Math.max(1, configs.getWorker().getExecuteStageWidth() / 5));
+      log.info(
+          String.format(
+              "executeReportResultWidth modified to %d",
+              configs.getWorker().getReportResultStageWidth()));
     }
   }
 
