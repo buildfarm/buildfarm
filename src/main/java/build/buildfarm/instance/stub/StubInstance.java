cfcfb2eb378f1fa92278fa73de1e2d2cabc0d111
Jason Schroeder <jschroeder@salesforce.com>
2025-10-03 14:25:35 -0700

    feat: worker shutdown pass-through

--- src/main/java/build/buildfarm/instance/stub/StubInstance.java
+++ src/main/java/build/buildfarm/instance/stub/StubInstance.java
@@ -1015,7 +1015,7 @@ public class StubInstance extends InstanceBase {
   }
 
   @Override
-  public PrepareWorkerForGracefulShutDownRequestResults shutDownWorkerGracefully() {
+  public PrepareWorkerForGracefulShutDownRequestResults shutDownWorkerGracefully(String name) {
     throwIfStopped();
     return workerControlBlockingStub
         .get()
