b99ec92c6b6fb2cfbb38e8215b53279a44e93250
Paco Sevilla <sevilla@deepdrive.tech>
2026-06-30 00:25:59 +0200

    Add actual tests

---
+++ e2e/output_directories/test_fruits.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+expected_files=("apple.txt" "banana.txt" "mango.txt")
+actual_files=($(ls -1 "fruits"))
+
+if [[ "${expected_files[*]}" != "${actual_files[*]}" ]]; then
+	echo "Expected files: ${expected_files[*]}" >&2
+	echo "Actual files: ${actual_files[*]}" >&2
+	exit 1
+fi
+
