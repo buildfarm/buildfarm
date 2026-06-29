df4f533e93f1149e528210e029e69b9eb26ead3f
Paco Sevilla <sevilla@deepdrive.tech>
2026-06-30 01:26:01 +0200

    Use locally built images

--- e2e/run_e2e_tests.sh
+++ e2e/run_e2e_tests.sh
@@ -2,11 +2,12 @@
 set -euo pipefail
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
+bazelisk run //container:buildfarm-server.load
+bazelisk run //container:buildfarm-worker.load
 
 setup() {
     (cd "$script_dir/.setup_buildfarm" && docker compose up --detach)
 }
-
 cleanup() {
     (cd "$script_dir/.setup_buildfarm" && docker compose down --volumes) || true
 }
@@ -18,7 +19,7 @@ for dir in "$script_dir"/*/; do
         trap cleanup EXIT
 
         cd "$dir"
-        bazel clean
-        bazel test --test_output=errors //...
+        bazelisk clean
+        bazelisk test --test_output=errors //...
     )
 done
